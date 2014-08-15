f4a078ab964a349138fee7e59dc1f2bcd7256d13
Evan R <eunoia.github@gmail.com>
2014-08-15 15:09:25 -0700

    Stubs of classes and pending tests

---
+++ spec/math_tables_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../lib/math_tables.rb'
+
+describe Eratosthenes do
+	it 'has tests' do
+		pending
+	end
+end
+
+describe Table do
+	it 'has tests' do
+		pending
+	end
+end
+
---
+++ spec/spec_helper.rb
@@ -0,0 +1,4 @@
+require 'resque_spec'
+
+RSpec.configure do |c|
+end
